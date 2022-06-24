08b54f2fa9db3a6109a32294db09785b869db7c2
4nZwar3 <4nzwar3@gmail.com>
2022-06-23 22:41:22 -0500

    first commit

---
+++ src/main/java/net/answare/telekinesismod/TelekinesisMod.java
@@ -0,0 +1,14 @@
+package net.answare.telekinesismod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TelekinesisMod implements ModInitializer {
+	private static final String TELEKINESISMOD = "telekinesismod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(TELEKINESISMOD);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/answare/telekinesismod/TelekinesisModClient.java
@@ -0,0 +1,10 @@
+package net.answare.telekinesismod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TelekinesisModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        
+    }
+}
